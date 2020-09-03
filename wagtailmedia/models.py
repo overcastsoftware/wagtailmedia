5e78f010b127396878fef9b66c1056cd6c532470
Kjartan Sverrisson <kjartan@overast.is>
2020-09-03 10:29:26 +0000

    class changes for Wagtail >= 2.9

--- wagtailmedia/models.py
+++ wagtailmedia/models.py
@@ -18,7 +18,11 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from taggit.managers import TaggableManager
-from wagtail.admin.utils import get_object_usage
+from wagtail.admin.models import get_object_usage
+# if WAGTAIL_VERSION < (2, 9):
+#     from wagtail.admin.utils import get_object_usage
+# else:
+#     from wagtail.admin.models import get_object_usage
 from wagtail.core.models import CollectionMember
 from wagtail.search import index
 from wagtail.search.queryset import SearchableQuerySetMixin
